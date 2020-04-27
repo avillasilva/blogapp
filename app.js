f55c09e7fe59edb5ac3ef8434603868a46b81b81
avillasilva <avillasilva@cc.ci.ufpb.br>
2020-04-27 20:49:18 -0300

    fourth commit

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.use('/admin', admin)
 app.use('/users', users)
 
 // Others
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log('The server is running.')
 })
