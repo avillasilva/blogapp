be046b63c4951c8361caa386652b785a3f244158
avillasilva <avillasilva@cc.ci.ufpb.br>
2020-04-27 20:32:46 -0300

    third commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV == 'production') {
-    module.exports = { mongoURI: 'mongodb+srv://Admin:<password>@cluster0-5j2rl.mongodb.net/test?retryWrites=true&w=majority'}
+if (process.env.NODE_ENV === 'production') {
+  module.exports = { mongoURI: 'mongodb+srv://Admin:4JIOltjuJLT0Cwy7@cluster0-5j2rl.mongodb.net/test?retryWrites=true&w=majority' }
 } else {
-    module.exports = { mongoURI: 'mongodb://localhost/blogapp' }
+  module.exports = { mongoURI: 'mongodb://localhost/blogapp' }
 }
